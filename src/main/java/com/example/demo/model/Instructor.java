d8ec7e8a01da65c4706ac12af1be873fc3aa9d22
unknown <CETECOM@AOAWSB02LC0727.duoc.acad>
2025-05-16 20:16:25 -0400

    primer commit

--- src/main/java/com/example/demo/model/Instructor.java
+++ src/main/java/com/example/demo/model/Instructor.java
@@ -1,5 +1,90 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_instructor;
+
+    private String nombre;
+    private String correo;
+    private String contraseña;
+    private String estado;
+    private String especialidad;
+    private String jornada;
+
+    // Constructor vacío
+    public Instructor() {}
+
+    public Instructor(String nombre, String correo, String contraseña, String estado, String especialidad, String jornada) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.estado = estado;
+        this.especialidad = especialidad;
+        this.jornada = jornada;
+    }
+
+    public Long getId_instructor() {
+        return id_instructor;
+    }
+
+    public void setId_instructor(Long id_instructor) {
+        this.id_instructor = id_instructor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getJornada() {
+        return jornada;
+    }
+
+    public void setJornada(String jornada) {
+        this.jornada = jornada;
+    }
 
 }
